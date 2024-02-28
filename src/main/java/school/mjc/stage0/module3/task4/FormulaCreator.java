7ce7692caf3e54eaa8193d28cf075e4044e100aa
Gempton <gemptonilya@gmail.com>
2024-02-28 21:30:32 +0300

    Done all tasks

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 2.56, b = 3.4;
+        double d;
+
+        d = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
+        System.out.println(d);
     }
 }
